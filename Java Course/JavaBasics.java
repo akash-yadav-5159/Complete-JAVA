a62429b5c5b107b8d115eee972db4ef1988c9641
akash-yadav-5159 <156719729+akash-yadav-5159@users.noreply.github.com>
2025-08-31 19:16:24 +0530

    Delete

--- Java Course/JavaBasics.java
+++
@@ -1,8 +0,0 @@
-public class JavaBasics {
-    public static void main(String args[]) {
-        System.out.println("Hello World");
-        System.out.print("kya bhai kya haal h");
-        //ln is used for next line like we use \n
-    }
-
-}
--- Java Course/Sum.java
+++
@@ -1,9 +0,0 @@
-public class Sum {
-
-    public static void main(String[] args) {
-        int a=21;
-        int b=33;
-        int sum=a+b;
-        System.out.print(sum);
-    }
-}
